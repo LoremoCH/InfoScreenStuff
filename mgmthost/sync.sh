9cf387a86f3f15ee43b4435a224a11ba30deda03
Jonas Meier <meiers.jonas@gmail.com>
2015-07-28 22:10:51 +0200

    add locking function, correct path

--- mgmthost/sync.sh
+++ mgmthost/sync.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 export host=192.168.1.150
 export remotedir=/home/player/Videos/
-export dir=/Users/X/InfoScreenFoyer
-export work=/users/X/.infoscreensync
+export dir=/Users/macmini/Desktop/InfoScreenFoyer
+export work=/users/macmini/.infoscreensync
 
 touch $work/sync.lock
+
+if [ -f $$work/sync.lock ]
+  then
+   echo "Sync is running..."
+   exit 0 
+fi
+
 
 shasum $dir/*.m4v > $work/current
 
@@ -12,7 +19,7 @@ cmp $work/current $work/last
 if [ $? -eq 1 ]; then
     scp -r $dir $host:$remotedir
     mv $work/current $work/last
-    ssh -c 'sudo reboot' $host
+    ssh -c 'sudo service lightdm restart' $host
 fi
 
 rm $work/sync.lock
